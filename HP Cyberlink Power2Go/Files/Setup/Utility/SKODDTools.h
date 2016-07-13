1f1784ef37a4d069cd43ee6fd70922378cbed633
casey.j.davis@gmail.com <casey.j.davis@gmail.com>
2016-07-13 08:25:12 -0600

    Initial commit

---
+++ HP Cyberlink Power2Go/Files/Setup/Utility/SKODDTools.h
@@ -0,0 +1,6 @@
+#ifndef _SK_ODD_TOOLS_H_
+#define _SK_ODD_TOOLS_H_
+
+extern "C" __declspec(dllexport) DWORD __stdcall HasDriveConnectedExWithLog(PBYTE pszVendID, DWORD dwVIDInByte, BOOL bSubStr, LPCTSTR szODDLogPath);
+
+#endif
