ebfd4fd63c1c0883ec4aa7dca01bf7d6c19e9883
casey.j.davis@gmail.com <casey.j.davis@gmail.com>
2016-07-13 10:56:50 -0600

    Altered gitignore file

--- HP Cyberlink Power2Go/Files/Setup/Utility/SKODDTools.h
+++
@@ -1,6 +0,0 @@
-#ifndef _SK_ODD_TOOLS_H_
-#define _SK_ODD_TOOLS_H_
-
-extern "C" __declspec(dllexport) DWORD __stdcall HasDriveConnectedExWithLog(PBYTE pszVendID, DWORD dwVIDInByte, BOOL bSubStr, LPCTSTR szODDLogPath);
-
-#endif
